5017169f2e8ff1256d13ff1b24e80ee95f68dac7
Darius Krištapavičius <darius@cuf.lt>
2014-09-04 19:57:32 +0300

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+#vagrant-verskis
+
+* CentOS 6.5
+* PHP 5.4
+* nginx (php-fpm)
+
+###Usage
+
+ * Create soft link for [project root] to ./www:
+
+ ```ln -s /home/user/www/capital-maps-api ./www```
+
+ * Up enviroment
+
+ ```vagrant up```
+
+ * Open project in browser:
+ 
+ http://api.maps.capital
+
+### Requirements
+
+ * Host unix-like OS (strongly recommended)
+ * Host NFS (```sudo apt-get install -y nfs-kernel-server```)
+ * Vagrant >=1.6.0
+ * Vagrant hostsupdater plugin (```vagrant plugin install vagrant-hostsupdater```)
+ * VirtualBox >=4.3.0
+
+### Known issues
+
+ Due to NFS project filesystem IO is 10%~70% slower (however default VirtualBox shared folders is ~2300% slower).
---
+++ bootstrap/bootstrap.sh
@@ -0,0 +1,54 @@
+# epel, remi
+sudo wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+sudo wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm;
+sudo rpm -ivh epel-release-6-8.noarch.rpm remi-release-6.rpm;
+sudo rm epel-release-6-8.noarch.rpm remi-release-6.rpm;
+
+sudo yum update -y;
+
+# config with enabled remi
+sudo rm /etc/yum.repos.d/remi.repo;
+sudo cp /bootstrap/etc/yum.repos.d/remi.repo /etc/yum.repos.d/remi.repo;
+
+# nginx
+sudo yum install nginx -y;
+
+# php
+sudo yum install php -y;
+
+# pphp-fpm
+sudo yum install php-fpm -y;
+
+# php.ini
+sudo rm /etc/php.ini;
+sudo cp /bootstrap/etc/php.ini /etc/php.ini;
+
+# nginx
+sudo rm /etc/nginx/conf.d/default.conf;
+sudo cp /bootstrap/etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf;
+
+# php extensions
+sudo yum install php-apc -y;
+sudo yum install php-intl -y;
+sudo yum install php-gd -y;
+sudo yum install php-exif -y;
+sudo yum install php-pdo -y;
+sudo yum install php-mysql -y;
+sudo yum install php-mbstring -y;
+sudo yum install php-common -y;
+sudo yum install php-mongo -y;
+
+# mysql
+sudo yum install mysql-client -y;
+
+# php-fpm autostart
+sudo chkconfig php-fpm on;
+
+# php-fpm start
+sudo service php-fpm start;
+
+# nginx autostart
+sudo chkconfig nginx on;
+
+# nginx start
+sudo service nginx start;
